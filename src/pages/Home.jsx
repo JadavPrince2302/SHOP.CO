a4f690639d37589c6fef6546b4581b0971003084
Jadav Prince <91061606+JadavPrince2302@users.noreply.github.com>
2025-09-16 03:49:55 +0530

    fix issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
     <>
       <div className="bg-[#F2F0F1] flex">
         <div className="max-w-[1240px] mx-auto py-8 sm:py-12 lg:py-[90px] px-4 sm:px-6 flex flex-col gap-6 sm:gap-8 lg:gap-11">
-          <div className="max-w-[577px] flex flex-col gap-6">
+          <div className="max-w-[577px] relative flex flex-col gap-6">
             <p className="font-integral text-[36px] sm:text-4xl md:text-5xl lg:text-[64px] font-black leading-[34px] sm:leading-tight lg:leading-[64px]">
               FIND CLOTHES THAT MATCHES YOUR STYLE
             </p>
@@ -37,6 +37,35 @@ const Home = () => {
               designed to bring out your individuality and cater to your sense
               of style.
             </div>
+            <div className="absolute -right-4 sm:-right-[50px] lg:-right-[100px] -top-5 lg:-top-[35px] hidden sm:block">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="80"
+                height="80"
+                viewBox="0 0 104 104"
+                fill="none"
+              >
+                <path
+                  d="M52 0C53.7654 27.955 76.0448 50.2347 104 52C76.0448 53.7654 53.7654 76.0448 52 104C50.2347 76.0448 27.955 53.7654 0 52C27.955 50.2347 50.2347 27.955 52 0Z"
+                  fill="black"
+                />
+              </svg>
+            </div>
+            <div className="absolute -left-10 sm:-left-[60px] lg:-left-[90px] bottom-[50px] sm:bottom-[80px] lg:bottom-[94px] hidden sm:block">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="48"
+                height="56"
+                viewBox="0 0 104 104"
+                fill="none"
+              >
+                <path
+                  d="M52 0C53.7654 27.955 76.0448 50.2347 104 52C76.0448 53.7654 53.7654 76.0448 52 104C50.2347 76.0448 27.955 53.7654 0 52C27.955 50.2347 50.2347 27.955 52 0Z"
+                  fill="black"
+                />
+              </svg>
+            </div>
+            <div>
             <Button
               variant="primary"
               size="lg"
@@ -44,6 +73,40 @@ const Home = () => {
             >
               Shop Now
             </Button>
+            </div>
+          </div>
+
+          <div className="flex flex-wrap sm:flex-row justify-center lg:justify-start gap-4 sm:gap-6 lg:gap-8 mt-8 sm:mt-6">
+            <div className="pl-0 sm:pl-3 text-center sm:text-left">
+              <div>
+                <span className="text-2xl sm:text-3xl md:text-[40px] font-satoshi font-black">
+                  200+
+                </span>
+              </div>
+              <div className="text-sm sm:text-[16px] font-satoshi opacity-60">
+                International Brands
+              </div>
+            </div>
+            <div className="pl-0 sm:pl-3 sm:border-l border-gray-300 text-center sm:text-left">
+              <div>
+                <span className="text-2xl sm:text-3xl md:text-[40px] font-satoshi font-black">
+                  2000+
+                </span>
+              </div>
+              <div className="text-sm sm:text-[16px] font-satoshi opacity-60">
+                High-Quality Products
+              </div>
+            </div>
+            <div className="pl-0 sm:pl-3 sm:border-l border-gray-300 text-center sm:text-left">
+              <div>
+                <span className="text-2xl sm:text-3xl md:text-[40px] font-satoshi font-black">
+                  30,000+
+                </span>
+              </div>
+              <div className="text-sm sm:text-[16px] font-satoshi opacity-60">
+                Happy Customers
+              </div>
+            </div>
           </div>
         </div>
       </div>
